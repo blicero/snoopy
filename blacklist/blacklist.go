da485ac074618563b39058ed864d178674275be3
Benjamin Walkenhorst <krylon@posteo.de>
2024-12-22 17:32:32 +0100

    Add package blacklist

---
+++ blacklist/blacklist.go
@@ -0,0 +1,56 @@
+// /home/krylon/go/src/github.com/blicero/snoopy/blacklist/blacklist.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 22. 12. 2024 by Benjamin Walkenhorst
+// (c) 2024 Benjamin Walkenhorst
+// Time-stamp: <2024-12-22 16:32:12 krylon>
+
+// Package blacklist provides a filter meant to exclude files from scanning.
+package blacklist
+
+import (
+	"regexp"
+
+	"github.com/gobwas/glob"
+)
+
+// Item is the common interface for several types of Blacklist patterns.
+type Item interface {
+	Match(path string) bool
+	HitCount() int64
+}
+
+// ReItem matches filenames against a regular expression.
+type ReItem struct {
+	Pattern *regexp.Regexp
+	Count   int64
+}
+
+func (i *ReItem) Match(path string) bool {
+	var hit = i.Pattern.MatchString(path)
+	if hit {
+		i.Count++
+	}
+	return hit
+} // func (i *ReItem) Match(path string) bool
+
+func (i *ReItem) HitCount() int64 {
+	return i.Count
+} // func (i *ReItem) HitCount() int64
+
+// GlobItem matches paths using the well-known globbing mechanism.
+type GlobItem struct {
+	Pattern glob.Glob
+	Count   int64
+}
+
+func (i *GlobItem) Match(path string) bool {
+	var hit = i.Pattern.Match(path)
+	if hit {
+		i.Count++
+	}
+	return hit
+} // func (i *GlobItem) Match(path string) bool
+
+func (i *GlobItem) HitCount() int64 {
+	return i.Count
+} // func (i *GlobItem) HitCount() int64
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/logutils v1.0.0
 )
+
+require github.com/gobwas/glob v0.2.3 // indirect
