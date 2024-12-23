f4aac82369c6a3c1c1423b5eac8d0c7fc8c978b9
Benjamin Walkenhorst <krylon@posteo.de>
2024-12-23 21:42:02 +0100

    Model: rename Folder to Root

--- model/model.go
+++ model/model.go
@@ -2,15 +2,15 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 22. 12. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-12-22 16:04:58 krylon>
+// Time-stamp: <2024-12-23 20:49:04 krylon>
 
 // Package model provides the data types used throughout the application.
 package model
 
 import "time"
 
-// Folder is a directory that is scanned for files
-type Folder struct {
+// Root is a directory that is scanned for files
+type Root struct {
 	ID       int64     `json:"id"`
 	Path     string    `json:"path"`
 	LastScan time.Time `json:"last_scan"`
@@ -19,7 +19,7 @@ type Folder struct {
 // File is a ... file.
 type File struct {
 	ID          int64     `json:"id"`
-	FolderID    int64     `json:"folder_id"`
+	RootID      int64     `json:"root_id"`
 	Path        string    `json:"path"`
 	Type        string    `json:"type"`
 	LastRefresh time.Time `json:"last_refresh"`
