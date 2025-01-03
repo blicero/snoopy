8826d449fc71388b99bd74b4b3d62495c20e264b
Benjamin Walkenhorst <krylon@posteo.de>
2025-01-03 23:05:33 +0100

    The main package now creates a GUI

--- main.go
+++ main.go
@@ -2,19 +2,37 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 23. 12. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-12-23 19:57:47 krylon>
+// Time-stamp: <2025-01-02 20:36:00 krylon>
 
 package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/blicero/snoopy/common"
+	"github.com/blicero/snoopy/ui"
 )
 
 func main() {
 	fmt.Printf("%s %s\n",
 		common.AppName,
 		common.Version)
-	fmt.Println("Implement me!!!")
+
+	defer fmt.Println("Bye Bye")
+
+	var (
+		err error
+		win *ui.SWin
+	)
+
+	if win, err = ui.Create(); err != nil {
+		fmt.Fprintf(
+			os.Stderr,
+			"Failed to create GUI: %s\n",
+			err.Error())
+		os.Exit(1)
+	}
+
+	win.Run()
 }
