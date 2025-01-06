b131616b2c14000a7e770b2e6f6919d5344ed6c9
Benjamin Walkenhorst <krylon@posteo.de>
2025-01-06 17:42:06 +0100

    Start fleshing out Walker

--- walker/walker.go
+++ walker/walker.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 23. 12. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-12-28 18:00:42 krylon>
+// Time-stamp: <2025-01-06 17:40:09 krylon>
 
 // Package walker implements the traversal of directories and the processing
 // of the files therein.
@@ -14,6 +14,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync/atomic"
+	"time"
 
 	"github.com/blicero/snoopy/blacklist"
 	"github.com/blicero/snoopy/common"
@@ -24,13 +26,19 @@ import (
 	"github.com/gabriel-vasile/mimetype"
 )
 
-const minSize = 4096 // nolint: unused,deadcode // 4 KiB, one page of virtual memory
+const (
+	minSize      = 4096            // nolint: unused,deadcode // 4 KiB, one page of virtual memory
+	scanInterval = time.Minute * 5 // XXX Set to more reasonable value after testing/debugging
+)
 
 // Walker traverses directory trees and processes the files it finds.
 type Walker struct {
-	log *log.Logger
-	bl  *blacklist.Blacklist
-	db  *database.Database
+	log    *log.Logger
+	bl     *blacklist.Blacklist
+	db     *database.Database
+	active atomic.Bool
+	visitQ chan *model.Root
+	ticker *time.Ticker
 }
 
 // NewWalker creates a new Walker instance that uses the given Blacklist.
@@ -56,13 +64,53 @@ func NewWalker(bl *blacklist.Blacklist) (*Walker, error) {
 			err.Error())
 	}
 
+	w.visitQ = make(chan *model.Root, 16)
+	w.ticker = time.NewTicker(time.Minute)
+	w.active.Store(true)
+	go w.loop()
+
 	return w, nil
 } // func NewWalker(bl *blacklist.Blacklist) (*Walker, error)
+
+// IsActive returns the Walker's Active flag
+func (w *Walker) IsActive() bool {
+	return w.active.Load()
+} // func (w *Walker) IsActive() bool
+
+// Stop clears the Walker's Active flag
+func (w *Walker) Stop() {
+	w.active.Store(false)
+} // func (w *Walker) Stop()
+
+// ScheduleScan adds the given Root to the Scan queue.
+func (w *Walker) ScheduleScan(r *model.Root) {
+	w.visitQ <- r
+} // func (w *Walker) ScheduleScan(r *model.Root)
+
+func (w *Walker) loop() {
+	defer w.log.Println("[INFO] Quitting Walker loop")
+	for w.IsActive() {
+		select {
+		case <-w.ticker.C:
+			continue
+		case r := <-w.visitQ:
+			w.Walk(r)
+		}
+	}
+} // func (w *Walker) loop()
 
 // Walk initiates a traversal of the given Directory tree
 func (w *Walker) Walk(r *model.Root) error {
 	w.log.Printf("[INFO] Walker about to traverse %s\n",
 		r.Path)
+	defer w.log.Printf("[INFO] Walker finished traversing %s\n", r.Path)
+
+	if r.LastScan.Add(scanInterval).After(time.Now()) {
+		w.log.Printf("[INFO] Directory %s is not due for another scan until %s\n",
+			r.Path,
+			r.LastScan.Add(scanInterval).Format(common.TimestampFormatMinute))
+		return nil
+	}
 
 	return filepath.WalkDir(r.Path, w.generateVisitorFunc(r))
 } // func (w *Walker) Walk(r *model.Root) error
@@ -85,6 +133,11 @@ func (w *Walker) generateVisitorFunc(r *model.Root) fs.WalkDirFunc {
 			return nil
 		} else if entry.IsDir() {
 			return nil
+		} else if info, err = entry.Info(); err != nil {
+			w.log.Printf("[ERROR] Cannot query FileInfo on %s: %s\n",
+				path,
+				err.Error())
+			return err
 		} else if f, err = w.db.FileGetByPath(path); err != nil {
 			w.log.Printf("[ERROR] Failed to look up file %s by path: %s\n",
 				path,
@@ -94,12 +147,7 @@ func (w *Walker) generateVisitorFunc(r *model.Root) fs.WalkDirFunc {
 			w.log.Printf("[TRACE] Add file %s to database...\n",
 				path)
 
-			if info, err = entry.Info(); err != nil {
-				w.log.Printf("[ERROR] Cannot query FileInfo on %s: %s\n",
-					path,
-					err.Error())
-				return err
-			} else if mtype, err = mimetype.DetectFile(path); err != nil {
+			if mtype, err = mimetype.DetectFile(path); err != nil {
 				w.log.Printf("[ERROR] Failed to determine MIME type for %s: %s\n",
 					path,
 					err.Error())
