43b66e2e51297ca5062dae9b0afc3468c691b4de
Benjamin Walkenhorst <krylon@posteo.de>
2025-09-12 19:27:54 +0200

    Add boilerplate

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-09-12 19:26:59 krylon>
+#
+# /data/code/python/snoopy/__init__.py
+# created on 12. 09. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+snoopy.__init__
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
